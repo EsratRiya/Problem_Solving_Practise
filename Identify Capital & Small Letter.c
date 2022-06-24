b6136679c1b5a70966f0da9b664d9a8c5dc21c47
EsratRiya <esratjahanriya1201@gmail.com>
2022-06-24 12:06:01 +0600

    Identifying letters added

---
+++ Identify Capital & Small Letter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char i;
+    scanf("%c",&i);
+
+    if(i>='a' && i<='z'){
+        printf("this is Small letter");
+    }
+
+    else{
+        printf("this is capital letter ");
+    }
+}
